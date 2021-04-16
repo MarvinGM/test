9caa7cd098f07020002fae74d7525d413f41c4ec
MarvinGM <marvinmahilum091197@gmail.com>
2021-04-16 21:35:56 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Amben on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
