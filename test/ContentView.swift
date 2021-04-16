a26881764371cd6bc34e88d1f4fa230652507fa6
MarvinGM <marvinmahilum091197@gmail.com>
2021-04-16 22:57:00 +0800

    Version 1.2

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, GITHUB world!")
+        Text("Hello, world!")
             .padding()
             .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
 }
 
+//MARK: - Section 2
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
